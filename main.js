af839c99c63bb87e349e9b7781b5a4507a76b1c0
Thomas-E-Johnson <thomas.e.johnson1@gmail.com>
2018-03-10 22:44:05 +0000

    Final Changes

---
+++ main.js
@@ -0,0 +1,30 @@
+const secondHand = document.querySelector(`.second-hand`);
+const minHand = document.querySelector(`.min-hand`);
+const hourHand = document.querySelector(`.hour-hand`);
+const allHands = document.querySelectorAll(`.hand`);
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minDegrees = ((mins / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  if (secondDegrees === 90) {
+    allHands.forEach(hand => hand.style.transition = `none`)
+  } else {
+    allHands.forEach(hand => hand.style.transition = ``)
+  }
+}
+
+setInterval(setDate, 1000);
+
+setDate();
